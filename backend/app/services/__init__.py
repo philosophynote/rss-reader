144c7a40e6ef47a129d25419700b462ddf5efdd7
nao <78991083+philosophynote@users.noreply.github.com>
2025-12-29 23:55:38 +0900

    test: キーワード管理のユニットテストを追加 (#10.3) (#20)

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -3,5 +3,6 @@
 from .article_service import ArticleService
 from .feed_fetcher_service import FeedFetcherService
 from .feed_service import FeedService
+from .keyword_service import KeywordService
 
-__all__ = ["ArticleService", "FeedFetcherService", "FeedService"]
+__all__ = ["ArticleService", "FeedFetcherService", "FeedService", "KeywordService"]
